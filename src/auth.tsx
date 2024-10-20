1ee99a113d78fe455e8d048eae65c189c013c252
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-10-20 22:06:15 +0300

    resetPassword functionality added

--- src/auth.tsx
+++ src/auth.tsx
@@ -1,34 +1,35 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { createUserWithEmailAndPassword, sendEmailVerification ,signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { auth } from './firebase';
+import { sendPasswordResetEmail } from 'firebase/auth';
 
 export const signup = async (email: string, password: string) => {
-  try {
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    return userCredential.user;
-  } catch (error) {
-    console.error('Error signing up:', error);
-    throw error;
-  }
+  const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+  await sendEmailVerification(userCredential.user);
+
+  return userCredential.user;
 };
 
 export const login = async (email: string, password: string) => {
-  try {
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
-    return userCredential.user;
-  } catch (error) {
-    console.error('Error logging in:', error);
-    throw error;
+  const userCredential = await signInWithEmailAndPassword(auth, email, password);
+  const user = userCredential.user;
+
+  if (!user.emailVerified) {
+    throw new Error('Verify your email address'); 
   }
+
+  return user;
 };
 
 export const signInWithGoogle = async () => {
-  const provider = new GoogleAuthProvider();
+  const provider = new GoogleAuthProvider(); 
+
   try {
     const result = await signInWithPopup(auth, provider);
-    return result.user;
+    const user = result.user;
+    return user;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
-    console.error('Error during Google Sign-In:', error);
-    throw error;
+    throw new Error('auth/google-sign-in-failed'); 
   }
 };
 
@@ -37,5 +38,16 @@ export const logout = async () => {
     await auth.signOut();
   } catch (error) {
     console.error('Error logging out:', error);
+  }
+};
+
+
+export const resetPassword = async (email: string) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    return 'Password reset email sent!';
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    throw new Error('Failed to send password reset email. Please try again.');
   }
 };
