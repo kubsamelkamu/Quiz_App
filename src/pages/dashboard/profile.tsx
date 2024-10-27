425bf68a92eb3d8cfe3b295a3376ea1321510778
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-10-27 23:35:58 +0300

    page to render preofile page

---
+++ src/pages/dashboard/profile.tsx
@@ -0,0 +1,12 @@
+import DashboardLayout from '@/components/Dashboard/DashboardLayout';
+import UserProfile from '@/components/Dashboard/UserProfile';
+
+const ProfilePage = () => {
+  return (
+    <DashboardLayout>
+      <UserProfile />
+    </DashboardLayout>
+  );
+};
+
+export default ProfilePage;
