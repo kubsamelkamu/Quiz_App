21b5482a1a0b1f3568488dd4e54be1892d720a86
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-10-28 23:55:38 +0300

    page to render QuizHistory

---
+++ src/pages/dashboard/history.tsx
@@ -0,0 +1,12 @@
+import DashboardLayout from '@/components/Dashboard/DashboardLayout';
+import QuizHistory from '@/components/Dashboard/QuizHistory';
+
+const QuizHistoryPage = () => {
+  return (
+    <DashboardLayout>
+      <QuizHistory />
+    </DashboardLayout>
+  );
+};
+
+export default QuizHistoryPage;
