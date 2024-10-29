f94cb597a11c1f5f161955b3295172fac162cd64
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-10-29 23:17:29 +0300

    render progressChart component

---
+++ src/pages/dashboard/progress.tsx
@@ -0,0 +1,12 @@
+import DashboardLayout from '@/components/Dashboard/DashboardLayout';
+import ProgressChart from '@/components/Dashboard/ProgressChart';
+
+const ProfilePage = () => {
+  return (
+    <DashboardLayout>
+      <ProgressChart/>
+    </DashboardLayout>
+  );
+};
+
+export default ProfilePage;
