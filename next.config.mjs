edb827b45d532cb3ae004b08d1ad8c8e3ae5c224
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-12-01 16:08:05 +0300

    include pwa to configuration list

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+import withPWA from 'next-pwa';
+
+const nextConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+
+})({
   reactStrictMode: true,
-};
+});
 
 export default nextConfig;
