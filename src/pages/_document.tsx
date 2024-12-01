e50bc0998854bab5f516c356fedd44818432887b
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-12-01 16:21:20 +0300

    link the manifest json

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+       <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/icons/quiz.png"/>
+        <meta name="theme-color" content="#0078d7" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
